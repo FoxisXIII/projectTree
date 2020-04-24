b355050396d621a27364546ea9cbdf91e9b675ce
Erik Espuñes Juberó <erik.espunyes7@outlook.com>
2020-04-24 17:31:47 +0200

    Enemy Spawn Done

---
+++ ProjectTree/Assets/Scripts/Spawners/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    private EntityManager _entityManager;
+    private Entity _enemyEntityPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        _enemyEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(enemyPrefab,
+            GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, new BlobAssetStore()));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.F))
+            SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        Entity enemy = _entityManager.Instantiate(_enemyEntityPrefab);
+        _entityManager.SetComponentData(enemy, new Translation() {Value = transform.position});
+        _entityManager.SetComponentData(enemy, new Rotation() {Value = Quaternion.identity});
+    }
+}
---
+++ ProjectTree/Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+[AlwaysSynchronizeSystem]
+public class MovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        float deltaTime = UnityEngine.Time.deltaTime;
+        Entities.ForEach((ref PhysicsVelocity velocity, in MovementData movementData) =>
+        {
+            velocity.Linear.z = -movementData.speed * deltaTime;
+        }).Run();
+        return default;
+    }
+}
