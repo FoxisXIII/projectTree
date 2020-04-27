1d3eca4058ea167c49f1962c0689734a48c77d47
foxisXIII <sofi.b.a97@gmail.com>
2020-04-27 19:37:05 +0200

    Base and GameController scripts added

---
+++ ProjectTree/Assets/Scripts/Base.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private int life;
+
+    //The base generates them by time?
+    private int energyCreation;
+    private int materialCreation;
+
+    public void receiveDamage(int damage)
+    {
+        life -= damage;
+        if(life<=0)
+            GameController.GetInstance().gameOver();
+    }
+}
---
+++ ProjectTree/Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController
+{
+    private static GameController instance;
+    
+    //private int waveNum;
+    private GameController()
+    {
+        //waveNum=0;
+    }
+
+    public static GameController GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameController();
+        }
+        return instance;
+    }
+
+    //TODO: Add the wavenumber/intensity of enemies/something?
+    public void initiateWave()
+    {
+        
+    }
+
+    public void endWave()
+    {
+        
+    }
+
+    public void pauseGame()
+    {
+        
+    }
+    
+    public void gameOver()
+    {
+        
+    }
+
+}
