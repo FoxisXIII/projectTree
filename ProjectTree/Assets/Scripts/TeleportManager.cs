069ef5c0077d41e4c6d1ebba38e1d161b4b51269
foxisXIII <sofi.b.a97@gmail.com>
2020-04-29 07:04:31 +0200

    Teleport scripts scheme done. Error with triggers

---
+++ ProjectTree/Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> teleporters;
+
+    [SerializeField]
+    private GameObject canvasMenu, player;
+    
+    private GameObject tpDestination;
+    public void teleport()
+    {
+        canvasMenu.SetActive(true);
+        tpElection();
+        player.transform.position = tpDestination.transform.position;
+        canvasMenu.SetActive(false);
+    }
+
+    private void tpElection()
+    {
+        
+        //tpDestination= whereClicked();
+    }
+}
---
+++ ProjectTree/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private TeleportManager TPManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print(other.name);
+        if (other.CompareTag("Teleporter") && Input.GetKeyDown(KeyCode.T))
+        {
+            print("Teleporting");
+            TPManager.teleport();
+        }
+    }
+}
