ee7d8a8445a1e2767b783e4ff0f51c777e449327
Loquele <joviedo@edu.tecnocampus.cat>
2020-04-29 12:01:34 +0200

    Puzzle 1 Done

---
+++ ProjectTree/Assets/Scripts/Puzzles/Validador.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Validador : MonoBehaviour
+{
+    public enum Solucion
+    {
+        RED=0,BLUE=1,GREEN=2
+    }
+
+    public Solucion B1, B2, B3;
+    public Material Mat;
+    public Solucion bt1, bt2, bt3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public  void Correccion()
+    {
+        if (B1==bt1)
+        {
+            if (B2==bt2)
+            {
+                if (B3==bt3)
+                {
+                    gameObject.GetComponent<MeshRenderer>().material = Mat;
+                    Debug.Log("Correcto");
+                }
+            }
+            
+        }
+    }
+}
