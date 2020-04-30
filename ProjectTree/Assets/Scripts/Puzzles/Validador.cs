67679828ae3388059067ad7af730aa540895b091
Loquele <joviedo@edu.tecnocampus.cat>
2020-04-30 20:16:03 +0200

    Puzzles acabados

--- ProjectTree/Assets/Scripts/Puzzles/Validador.cs
+++ ProjectTree/Assets/Scripts/Puzzles/Validador.cs
@@ -12,17 +12,36 @@ public class Validador : MonoBehaviour
     public Solucion B1, B2, B3;
     public Material Mat;
     public Solucion bt1, bt2, bt3;
+    public GameObject casco;
+    
+    public float time, speed;
+    private bool now=false;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("move",time);
+    }
+
+    void move()
+    {
+        Debug.Log("10Seg para auto destruccion" );
+        now = true;
+        //casco.GetComponent<BoxCollider>().isTrigger = true;
+        //casco.SetActive(false);
+        Destroy(casco,5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (now && casco.transform.position.y!=20f)
+        {
+            Debug.Log("Arriba plataforma");
+            Vector3 newp = Vector3.MoveTowards(casco.transform.position,
+                new Vector3(casco.transform.position.x, 20, casco.transform.position.z), speed * Time.deltaTime);
+            casco.transform.position = newp;
+        }
     }
 
     public  void Correccion()
