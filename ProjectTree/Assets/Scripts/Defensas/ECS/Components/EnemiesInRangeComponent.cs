1e3cc401c49928971619abbd5bc9e675bf85bab2
sirYabar <myabar@edu.tecnocampus.cat>
2020-05-02 01:36:00 +0200

    no funca, null reference en la torreta, creo que en el enemiesInRange component

--- ProjectTree/Assets/Scripts/Defensas/ECS/Components/EnemiesInRangeComponent.cs
+++ ProjectTree/Assets/Scripts/Defensas/ECS/Components/EnemiesInRangeComponent.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Entities;
+using UnityEngine;
 
+[InternalBufferCapacity(20)]
 [Serializable]
 [GenerateAuthoringComponent]
 public struct EnemiesInRange : IBufferElementData
 {
-    public Entity enemies;
+    public Entity Value;
 }
