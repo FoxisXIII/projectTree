f76af27b7d8a41a989ab47dd34bf05b98a151ec9
Erik Espuñes Juberó <erik.espunyes7@outlook.com>
2020-05-18 17:29:31 +0200

    DONE

--- ProjectTree/Assets/Scripts/Defensas/Projectile/ProjectileBehaviour.cs
+++ ProjectTree/Assets/Scripts/Defensas/Projectile/ProjectileBehaviour.cs
@@ -5,38 +5,18 @@ using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class ProjectileBehaviour : MonoBehaviour, IConvertGameObjectToEntity
 {
     public float speed = 50f;
 
     public float lifeTime = 1f;
-
-    private Rigidbody bulletRigidBody;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        bulletRigidBody = GetComponent<Rigidbody>();
-        Invoke("RemoveProjectile", lifeTime);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 movement = Time.deltaTime * speed * transform.forward;
-        bulletRigidBody.MovePosition(transform.position + movement);
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        RemoveProjectile();
-    }
-
-    void RemoveProjectile()
-    {
-        Destroy(gameObject);
-    }
+    
     
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
