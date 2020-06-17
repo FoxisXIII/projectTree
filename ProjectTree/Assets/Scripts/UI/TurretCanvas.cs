553d4d25f9dd7a00f981afae5f77e8508befb493
sirYabar <myabar@edu.tecnocampus.cat>
2020-06-17 12:42:16 +0200

    puestos tooltips de torretas

---
+++ ProjectTree/Assets/Scripts/UI/TurretCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretCanvas : MonoBehaviour
+{
+    public GameObject TooltipBox;
+    private void OnEnable()
+    {
+        TooltipBox.SetActive(true);
+    }
+}
